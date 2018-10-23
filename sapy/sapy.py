43b83c24c452030dc282263c4cf2a627f416b267
y2k <git@y2kbugger.com>
2018-10-22 23:52:44 -0400

    Add each component tested in isolation

--- sapy/sapy.py
+++ sapy/sapy.py
@@ -1,2 +1,203 @@
+class ProgramCounter():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.counter = 0
+
+    def clock(self, cp=False, **kwargs):
+        """
+        cp : bool
+            Whether to increment program counter
+        """
+        if cp:
+            self.counter += 1
+
+    def data(self, ep=False, **kwargs):
+        if ep:
+            return self.counter
+        else:
+            return None
+
+class MemoryAddressRegister():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._address = 0x0
+
+    def data(self, **kwargs):
+        # mar never outputs to the bus
+        return None
+
+    def clock(self, data=None, lm=False, **kwargs):
+        if data > 0xF:
+            raise ValueError("Address bus limited to 4 bit")
+        if lm:
+            self._address = data
+
+    def address(self):
+        return self._address
+
+class RandomAccessMemory():
+    def __init__(self, mar):
+        self._mar = mar
+        self.reset()
+
+    def reset(self):
+        self.values = {x: 0x00 for x in range(0xf + 1)} # 16 total values
+
+    def clock(self, data=None,  lr=False, **kwargs):
+        if lr:
+            assert not data is None
+            self.values[self._mar.address()] = data
+
+    def data(self, er=False, **kwargs):
+        if er:
+            return self.values[self._mar.address()]
+        else:
+            return None
+
+class SwitchBoard():
+    def __init__(self, ram, mar):
+        self._ram = ram
+        self._mar = mar
+        self.reset()
+
+    def reset(self):
+        self.address = 0x0
+        self.data = 0x00
+
+    def load_program(self, program):
+        self.address = 0x0
+        for opcode in program:
+            self.data = opcode
+            self.write_ram()
+            self.address += 1
+
+    def write_ram(self):
+        # store address for ram in register
+        self._mar.clock(data=self.address, lm=True)
+        # clock data into ram at the address set above
+        self._ram.clock(data=self.data, lr=True)
+
+class RegisterA():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = 0x00
+
+    def clock(self, data=None,  la=False, **kwargs):
+        if la:
+            assert not data is None
+            assert 0x00 <= data <= 0xFF
+            self.value = data
+
+    def data(self, ea=False, **kwargs):
+        if ea:
+            return self.value
+
+class RegisterB():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = 0x00
+
+    def clock(self, data=None,  lb=False, **kwargs):
+        if lb:
+            assert not data is None
+            assert 0x00 <= data <= 0xFF
+            self.value = data
+
+    def data(self, **kwargs):
+        return None
+
+class RegisterOutput():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = 0x00
+
+    def clock(self, data=None,  lo=False, **kwargs):
+        if lo:
+            assert not data is None
+            assert 0x00 <= data <= 0xFF
+            self.value = data
+            print(f"Output: {self.value}")
+
+    def data(self, **kwargs):
+        return None
+
+class ArithmeticUnit():
+    def __init__(self, accumulator, reg_b):
+        self.accumulator = accumulator
+        self.reg_b = reg_b
+        self.reset()
+
+    def reset(self):
+        pass
+
+    def data(self, eu=False, su=False, **kwargs):
+        if not eu:
+            return None
+
+        if not su:
+            a = self.accumulator.value + self.reg_b.value
+        elif su:
+            a =  self.accumulator.value - self.reg_b.value
+
+        # Handel overflows
+        base = 1 << 8 # eight bits
+        return a % base
+
+class RegisterInstruction():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = 0x00
+
+    def clock(self, data=None,  li=False, **kwargs):
+        if li:
+            assert not data is None
+            assert 0x00 <= data <= 0xFF
+            self.value = data
+
+    def data(self, ei=False, **kwargs):
+        if ei:
+            # return self.value
+            return self.value & 0x0F # low nibble is the argument that goes to the bus
+
+    def opcode(self):
+        return (self.value & 0xF0) >> 4 # high nibble is the opcode
+
 class Clock():
-    pass
+    def __init__(self):
+        self.components = []
+
+    def reset(self):
+        for c in self.components:
+            c.reset()
+
+    def add_component(self, component):
+        self.components.append(component)
+
+    def data_bus(self, control_word):
+        datas = []
+        for c in self.components:
+            d = c.data(**control_word)
+            if not d is None:
+                datas.append(d)
+        print(datas)
+        print(self.components)
+        if len(datas) == 1:
+            return datas[0]
+        elif len(datas) == 0:
+            raise RuntimeError("Nothing outputting to the data bus for this control word")
+        elif len(datas) > 1:
+            raise RuntimeError("More than one component outputting to the data bus")
+
+
--- sapy/test_sapy.py
+++ sapy/test_sapy.py
@@ -1,3 +1,229 @@
-from sapy import Clock
-def test_clock():
+import pytest
+
+from sapy import Clock, ProgramCounter, MemoryAddressRegister, RandomAccessMemory, SwitchBoard, RegisterA, RegisterB, RegisterOutput, ArithmeticUnit, RegisterInstruction
+
+def test_program_counter_increments():
+    pc = ProgramCounter()
+
+    pc.clock()
+    assert pc.data(ep=True) == 0x00
+
+    pc.clock(cp=True)
+    assert pc.data(ep=True) == 0x01
+
+    pc.clock(cp=True)
+    assert pc.data(ep=True) == 0x02
+
+def test_program_counter_resets():
+    pc = ProgramCounter()
+
+    pc.clock(cp=True)
+    assert pc.data(ep=True) == 0x01
+
+    pc.reset()
+    assert pc.data(ep=True) == 0x00
+
+def test_program_counter_needs_ep():
+    pc = ProgramCounter()
+
+    pc.clock(cp=True)
+    assert pc.data() == None
+
+    pc.reset()
+    assert pc.data() == None
+
+def test_memory_address_register_latches_data_on_lm():
+    mar = MemoryAddressRegister()
+
+    assert mar.data() == None
+
+    mar.clock(data=0xF, lm=True)
+    assert mar.address() == 0xF
+
+    mar.clock(data=0x3, lm=True)
+    assert mar.address() == 0x3
+
+    mar.clock(data=0xC, lm=False)
+    assert mar.address() == 0x3
+
+
+def test_ram_can_store_values():
+    mar = MemoryAddressRegister()
+    ram = RandomAccessMemory(mar)
+
+    # store address for ram in register
+    mar.clock(data=0xF, lm=True)
+    assert mar.address() == 0xF
+
+    # clock data into ram at the address set above
+    ram.clock(data=0xAB, lr=True)
+    assert ram.data() == None
+    assert ram.data(er=True) == 0xAB
+
+    # don't clock data into ram at the address set above
+    ram.clock(data=0xAA)
+    assert ram.data(er=True) == 0xAB
+
+def test_addresses_must_be_4_bit():
+    mar = MemoryAddressRegister()
+    ram = RandomAccessMemory(mar)
+
+    fourbitmax = 0xF
+
+    # store address for ram in register
+    mar.clock(data=fourbitmax, lm=True)
+    assert mar.address() == fourbitmax
+
+    with pytest.raises(ValueError):
+        mar.clock(data=fourbitmax + 1, lm=True)
+
+
+def test_switches_can_give_address_and_data():
+    switches = SwitchBoard(ram=None, mar=None)
+    assert switches.address == 0x0
+    assert switches.data == 0x00
+
+def test_switches_can_initialize_ram():
+    program = [0xFA, 0x12]
+
+    mar = MemoryAddressRegister()
+    ram = RandomAccessMemory(mar)
+    switches = SwitchBoard(ram, mar)
+
+    switches.load_program(program)
+
+    # set ram address
+    mar.clock(data=0x0, lm=True)
+    assert ram.data(er=True) == 0xFA
+    mar.clock(data=0x1, lm=True)
+    assert ram.data(er=True) == 0x12
+    mar.clock(data=0x0, lm=True)
+    assert ram.data(er=True) == 0xFA
+
+def test_a_register_can_store_and_retrieve_values():
+    reg_a = RegisterA()
+
+    assert reg_a.data(ea=True) == 0x00
+    reg_a.clock(data=0xAB, la=True)
+    assert reg_a.value == 0xAB
+    assert reg_a.data() == None
+    assert reg_a.data(ea=True) == 0xAB
+
+def test_b_register_can_store_and_not_retrieve():
+    reg_b = RegisterB()
+
+    # register_b cannot put data on the bus
+    assert reg_b.data(eb=True) == None
+
+    reg_b.clock(data=0xAB, lb=True)
+    assert reg_b.value == 0xAB
+    assert reg_b.data() == None
+
+    # register_b cannot put data on the bus
+    assert reg_b.data(eb=True) == None
+
+
+def test_output_register_can_store_and_not_retrieve():
+    reg_o = RegisterOutput()
+
+    # register_o cannot put data on the bus
+    assert reg_o.data(eo=True) == None
+
+    reg_o.clock(data=0xAB, lo=True)
+    assert reg_o.value == 0xAB
+    assert reg_o.data() == None
+
+    # register_o cannot put data on the bus
+    assert reg_o.data(eo=True) == None
+
+@pytest.mark.parametrize("a,b,expected", [
+    (0x00, 0x00, 0x00),
+    (0x03, 0x04, 0x07),
+    (0xFF, 0x01, 0x00), # overflow
+    ])
+def test_arithmetic_unit_adds(a, b, expected):
+    reg_a = RegisterA()
+    reg_a.clock(data=a, la=True)
+
+    reg_b = RegisterB()
+    reg_b.clock(data=b, lb=True)
+
+    adder = ArithmeticUnit(reg_a, reg_b)
+
+    assert adder.data() is None
+    assert adder.data(eu=True) == expected
+
+@pytest.mark.parametrize("a,b,expected", [
+    (0x04, 0x04, 0x00),
+    (0x04, 0x03, 0x01),
+    (0x00, 0x01, 0xFF), # underflow
+    ])
+def test_arithmetic_unit_subtracts(a, b, expected):
+    reg_a = RegisterA()
+    reg_a.clock(data=a, la=True)
+
+    reg_b = RegisterB()
+    reg_b.clock(data=b, lb=True)
+
+    adder = ArithmeticUnit(reg_a, reg_b)
+
+    assert adder.data() is None
+    assert adder.data(eu=True, su=True) == expected
+
+def test_instruction_register_splits_value():
+    reg_i = RegisterInstruction()
+    instruction = 0xCD # both opcode and argument, 8bits
+    reg_i.clock(data=instruction, li=True)
+    assert reg_i.value == instruction
+    assert reg_i.opcode() == 0xC # opcode
+    assert reg_i.data() == None
+    assert reg_i.data(ei=True) == 0xD # address
+
+def test_clock_add_component():
     clock = Clock()
+    pc = ProgramCounter()
+    clock.add_component(pc)
+
+def test_clock_gets_data():
+    # pull data from connected components from data bus
+    clock = Clock()
+    reg_a = RegisterA()
+    clock.add_component(reg_a)
+
+    reg_a.clock(data=0xCD, la=True)
+
+    control_word = {'ea': True}
+
+    assert clock.data_bus(control_word) == 0xCD
+
+def test_clock_fails_if_multi_data_accessed():
+    # pull data from connected components from data bus
+    clock = Clock()
+    reg_a = RegisterA()
+    pc = ProgramCounter()
+    clock.add_component(reg_a)
+    clock.add_component(pc)
+
+    reg_a.clock(data=0xCD, la=True)
+    pc.clock(data=0xAE, ep=True)
+
+    control_word = {'ea': True, 'ep':True}
+
+    with pytest.raises(RuntimeError):
+        clock.data_bus(control_word)
+
+def test_clock_resets_components():
+    class SpyComponent():
+        def __init__(self):
+            self.reset_called = False
+
+        def reset(self):
+            self.reset_called = True
+
+    c = SpyComponent()
+    clock = Clock()
+
+    clock.add_component(c)
+
+    clock.reset()
+    assert c.reset_called
