87ee75429336464b20e2da1d51eec15f480eb812
y2k <git@y2kbugger.com>
2018-11-25 23:40:20 -0500

    Continue work on the assembler

--- sapy/assembler.py
+++ sapy/assembler.py
@@ -1,13 +1,9 @@
 from typing import List
 
-MNEMONIC = {
-    'LDA': 0x00,
-    'ADD': 0x01,
-    'SUB': 0x02,
-    'OUT': 0x03,
-    'JMP': 0x04,
-    'HLT': 0xFF,
-    }
+from .sapy import opcode_map, mnemonics, implied, absolute, absolute_branching, indirect, indirect_branching, immediate
+
+
+MNEMONIC = {m.mnemonic:m for m in mnemonics}
 
 def assemble(instructions) -> List[int]:
     bytecode = []
@@ -16,8 +12,41 @@ def assemble(instructions) -> List[int]:
     return bytecode
 
 def translate_instruction(instruction: str) -> List[int]:
-    if ' ' in instruction:
-        mne, arg = instruction.split(' ', 1)
-    else:
-        mne = instruction.split(' ', 1)[0]
-    return [MNEMONIC[mne]]
+    if not ' ' in instruction:
+        # implied addressing
+        opcode = MNEMONIC[instruction]
+        return [(implied.high_nibble << 4) + opcode.low_nibble]
+
+    mne_chars, arg = instruction.split(' ', 1)
+    mne = MNEMONIC[mne_chars]
+
+    # Should be one or the other
+    # This could only go wrong with misconfigured mnemonics
+    ads = mne.addressing_modes
+    assert not ((indirect in ads) and (indirect_branching in ads))
+    assert not ((absolute in ads) and (indirect_branching in ads))
+    assert not ((absolute_branching in ads) and (indirect in ads))
+    assert not ((absolute_branching in ads) and (absolute in ads))
+
+    data_addressing_modes = [immediate, absolute, indirect]
+    branching_addressing_modes = [absolute_branching, indirect_branching]
+
+    if any(ad in data_addressing_modes for ad in ads):
+        if arg[0] == '$':
+            arg_addressing_mode = absolute
+            arg_val = int(arg[1:], 16)
+        elif arg[0] == '#' and arg[1] == '$': 
+            arg_val = int(arg[2:], 16)
+            arg_addressing_mode = immediate
+        elif arg[0] == '(' and arg[1] == '$' and arg[-1] == ')': 
+            arg_val = int(arg[2:-1], 16)
+            arg_addressing_mode = indirect
+    if any(ad in branching_addressing_modes for ad in ads):
+        if arg[0] == '$':
+            arg_addressing_mode = absolute_branching
+            arg_val = int(arg[1:], 16)
+        elif arg[0] == '(' and arg[1] == '$' and arg[-1] == ')': 
+            arg_val = int(arg[2:-1], 16)
+            arg_addressing_mode = indirect_branching
+    assert arg_val <= 0xFF
+    return [(arg_addressing_mode.high_nibble << 4) + mne.low_nibble, arg_val]
--- sapy/test_assemble.py
+++ sapy/test_assemble.py
@@ -1,14 +1,33 @@
 from .assembler import MNEMONIC as M, assemble
 
-def test_parameterless():
+def test_implied():
     instructions = ['HLT']
     bytecode = assemble(instructions)
-    assert bytecode == [M['HLT']]
+    assert bytecode == [0xFF]
 
+def test_immediate():
+    instructions = ['LDA #$C2']
+    bytecode = assemble(instructions)
+    assert bytecode == [0x20, 0xC2]
 
 def test_absolute():
     instructions = ['LDA $C2']
     bytecode = assemble(instructions)
-    assert bytecode == [M['LDA'], 0xC2]
+    assert bytecode == [0x00, 0xC2]
+
+def test_indirect():
+    instructions = ['LDA ($C2)']
+    bytecode = assemble(instructions)
+    assert bytecode == [0x10, 0xC2]
+
+def test_absolute_branching():
+    instructions = ['JMP $C2']
+    bytecode = assemble(instructions)
+    assert bytecode == [0x34, 0xC2]
+
+def test_indirect_branching():
+    instructions = ['JMP ($C2)']
+    bytecode = assemble(instructions)
+    assert bytecode == [0x44, 0xC2]
 
 # def test_missing_arg_raises()
